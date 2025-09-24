318da1ab0624044608c405b61fc0206bfccaf162
slamduncaan747 <43890142+slamduncaan747@users.noreply.github.com>
2025-09-24 12:35:41 -0400

    Massive training improvements. Added dropout, and increased weight decay to fight against overfitting, and gradient clipping + learning rate scheduler to avoid exploding gradients. Also made other improvements in logging such as Weights and Biases integration, and improved checkpoint saving.

--- model.py
+++ model.py
@@ -1,23 +1,26 @@
 import torch
 
 class BachNet(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, dropout_rate=.2):
         super(BachNet, self).__init__()
-        self.double_conv_1 = DoubleConvBlock(1, 64)
-        self.encoder_1 = EncoderBlock(64, 128)
-        self.encoder_2 = EncoderBlock(128, 256)
-        self.encoder_3 = EncoderBlock(256, 512)
+        self.dropout_rate = dropout_rate
+        self.double_conv_1 = DoubleConvBlock(1, 64, dropout_rate=self.dropout_rate)
+        self.encoder_1 = EncoderBlock(64, 128, dropout_rate=self.dropout_rate)
+        self.encoder_2 = EncoderBlock(128, 256, dropout_rate=self.dropout_rate)
+        self.encoder_3 = EncoderBlock(256, 512, dropout_rate=self.dropout_rate)
 
         self.bottleneck = DoubleConvBlock(512, 1024)
 
-        self.decoder_1 = DecoderBlock(1024, 512)
-        self.decoder_2 = DecoderBlock(512, 256)
-        self.decoder_3 = DecoderBlock(256, 128)
-        self.decoder_4 = DecoderBlock(128, 64)
+        self.decoder_1 = DecoderBlock(1024, 512, dropout_rate=self.dropout_rate)
+        self.decoder_2 = DecoderBlock(512, 256, dropout_rate=self.dropout_rate)
+        self.decoder_3 = DecoderBlock(256, 128, dropout_rate=self.dropout_rate)
+        self.decoder_4 = DecoderBlock(128, 64, dropout_rate=self.dropout_rate)
 
         self.output_1 = torch.nn.Conv2d(64, 1, kernel_size=1)
         self.output_2 = torch.nn.Conv2d(64, 1, kernel_size=1)
         self.output_3 = torch.nn.Conv2d(64, 1, kernel_size=1)
+
+
     def forward(self, x):
         skip1 = self.double_conv_1(x)
         downsampled_1, skip2 = self.encoder_1(skip1)
@@ -31,49 +34,57 @@ class BachNet(torch.nn.Module):
         upsampled_3 = self.decoder_3(upsampled_2, skip2)
         decoder_output = self.decoder_4(upsampled_3, skip1)
 
-        output_1 = self.output_1(decoder_output)
-        output_2 = self.output_2(decoder_output)
-        upsampled_output_3 = torch.nn.functional.interpolate(decoder_output, size=(28, 64), mode='bilinear', align_corners=False)
-        output_3 = self.output_3(upsampled_output_3)
+        resized_for_1_2 = torch.nn.functional.interpolate(decoder_output, size=(21, 64), mode='bilinear', align_corners=False)
+        output_1 = self.output_1(resized_for_1_2)
+        output_2 = self.output_2(resized_for_1_2)
+
+        resized_for_3 = torch.nn.functional.interpolate(decoder_output, size=(28, 64), mode='bilinear', align_corners=False)
+        output_3 = self.output_3(resized_for_3)
+
         return output_1, output_2, output_3
-
-        
-
 
 
 class DoubleConvBlock(torch.nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, dropout_rate=0.2):
         super(DoubleConvBlock, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
+        self.dropout = torch.nn.Dropout2d(p=dropout_rate)
         self.conv1 = torch.nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
         self.conv2 = torch.nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
         self.relu = torch.nn.ReLU()
+
     def forward(self, x):
         x = self.conv1(x)
         x = self.relu(x)
+        x = self.dropout(x)
         x = self.conv2(x)
         x = self.relu(x)
         return x
 
+
 class EncoderBlock(torch.nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, dropout_rate=0.2):
         super(EncoderBlock, self).__init__()
-        self.double_conv = DoubleConvBlock(in_channels, out_channels)
+        self.double_conv = DoubleConvBlock(in_channels, out_channels, dropout_rate=dropout_rate)
         self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+
     def forward(self, x):
         skip_connection_tensor = self.double_conv(x)
         downsampled_tensor = self.pool(skip_connection_tensor)
         return downsampled_tensor, skip_connection_tensor
 
+
 class DecoderBlock(torch.nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, dropout_rate=0.2):
         super(DecoderBlock, self).__init__()
-        self.conv1 = torch.nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
-        self.double_conv = DoubleConvBlock(in_channels*2, out_channels)
+        self.conv1 = torch.nn.ConvTranspose2d(in_channels, in_channels//2, kernel_size=2, stride=2)
+        self.double_conv = DoubleConvBlock(in_channels, out_channels, dropout_rate=dropout_rate)
+    
     def forward(self, x, skip_connection_tensor):
         upsampled_tensor = self.conv1(x)
-        combined_tensor = torch.cat((upsampled_tensor, skip_connection_tensor), dim=1)
+        resized_skip = torch.nn.functional.interpolate(skip_connection_tensor, size=upsampled_tensor.shape[2:])
+        combined_tensor = torch.cat((upsampled_tensor, resized_skip), dim=1)
         feature_map = self.double_conv(combined_tensor)
         return feature_map
 
--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import torch
 from torch.utils.data import DataLoader, TensorDataset, random_split
 from model import BachNet
 from ChoraleDataset import ChoraleDataset
+import os
 
 if torch.cuda.is_available():
     device = torch.device("cuda")
@@ -12,24 +13,47 @@ elif torch.backends.mps.is_available():
 else:
     device = torch.device("cpu")
 torch.manual_seed(8)
-with open('dataset.pkl', 'rb') as f:
-    dataset = pickle.load(f)
+
+
 
 # Hyperparameters
 epochs = 1000
 
-checkpoint_iters = 100
+checkpoint_iters = 50
 val_iters = 5
+increased_checkpoint_epoch = 600
+increased_checkpoint_iters = 10
 
 val_ratio = .1
-batch_size = 4096
+batch_size = 1024
 
-learning_rate = 0.002
+learning_rate = 0.004
 dropout_rate = .1
-weight_decay = 1e-5
+weight_decay = 2e-5
+
+from_checkpoint = True
+checkpoint_path = '../content/drive/MyDrive/BachNet/checkpoints/'
+best_checkpoint_path = '../content/drive/MyDrive/BachNet/checkpoints/best/'
+checkpoint_epoch = 600
+
+# Initialize Logging
+wandb.init(
+    project="Bach-Net",
+    config={
+        "epochs": epochs,
+        "checkpoint_iters": checkpoint_iters,
+        "val_iters": val_iters,
+        "val_ratio": val_ratio,
+        "batch_size": batch_size,
+        "learning_rate": learning_rate,
+        "weight_decay":  weight_decay,
+        "dropout_rate": .1,
+    }
+)
+config = wandb.config
 
 # Create data loaders
-t_set = ChoraleDataset('dataset.pkl')
+t_set = ChoraleDataset('Bach-Net/dataset.pkl')
 t_size = int(len(t_set)*(1-val_ratio))
 v_size = int(len(t_set)-t_size)
 train_set, val_set = random_split(t_set, [t_size, v_size])
@@ -38,16 +62,32 @@ val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False)
 
 # Define model, loss function, and optimizer
 model = BachNet()
+start_epoch = 0
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=config.epochs, eta_min=1e-6)
+
+if from_checkpoint:
+    full_path = os.path.join(checkpoint_path, checkpoint_name)
+    checkpoint = torch.load(full_path, map_location=device)
+    if(isinstance(checkpoint, dict) and 'model_state_dict' in checkpoint):
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        start_epoch = checkpoint['epoch']
+    else:
+        model.load_state_dict(checkpoint)
+        start_epoch = checkpoint_epoch
+
 model.to(device)
 loss_function = torch.nn.BCEWithLogitsLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+wandb.watch(model, log='all', log_freq=100)
 
+top5 = []
 # Training loop
-for epoch in range(epochs):
+for epoch in range(start_epoch, epochs):
     total_loss = 0
     # Train on train_set
+    model.train()
     for x, y_1, y_2, y_3 in train_loader:
-        model.train()
         x = x.to(device)
         y_1, y_2, y_3 = y_1.to(device), y_2.to(device), y_3.to(device)
         
@@ -57,21 +97,17 @@ for epoch in range(epochs):
         loss_1, loss_2, loss_3 = loss_function(output_1, y_1), loss_function(output_2, y_2), loss_function(output_3, y_3)
         final_loss = loss_1 + loss_2 + loss_3
         final_loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         optimizer.step()
         total_loss += final_loss.item()
     total_loss /= len(train_loader)
-
-    # Save checkpoint at intervals
-    if (epoch+1) % checkpoint_iters == 0:
-        path = f'checkpoints/checkpoint{epoch+1}.pth'
-        torch.save(model.state_dict(), path)
     
     # Get validation loss at intervals
+    total_val_loss = 0
     if (epoch+1) % val_iters == 0:
-        total_val_loss = 0
+        model.eval()
         with torch.no_grad():
-            for x_val, y_val_1, y_val_2, y_val_3 in val_loader:
-                mode.eval()
+            for x_val, y_val_1, y_val_2, y_val_3 in val_loader:    
                 x_val = x_val.to(device)
                 y_val_1, y_val_2, y_val_3 = y_val_1.to(device), y_val_2.to(device), y_val_3.to(device)
                 logits_val_1, logits_val_2, logits_val_3 = model(x_val)
@@ -80,5 +116,42 @@ for epoch in range(epochs):
                 total_val_loss += loss_val.item()
         total_val_loss /= len(val_loader)
         print(f"Epoch [{epoch+1}/{epochs}], Loss: {total_loss:.4f}, Val Loss: {total_val_loss:.4f}")
+        wandb.log({
+            "epoch": epoch + 1,
+            "train_loss": total_loss,
+            "val_loss": total_val_loss,
+            "learning_rate": scheduler.get_last_lr()[0]
+        })
     else:
         print(f"Epoch [{epoch+1}/{epochs}], Loss: {total_loss:.4f}")
+        wandb.log({"epoch": epoch + 1, "train_loss": total_loss})
+    # Save checkpoint at intervals
+    if (epoch+1) % (checkpoint_iters) == 0:
+        os.makedirs('checkpoints', exist_ok=True)
+        path = f'checkpoints/checkpoint{epoch+1}.pth'
+        torch.save({
+            'epoch': epoch + 1,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+        }, path)
+    
+    if (epoch+1) % (increased_checkpoint_iters) == 0 and epoch+1 >= increased_checkpoint_epoch:
+        top5last = float('inf')
+        if len(top5) == 5:
+            top5.sort(key=lambda x: x[0])
+            top5last = top5[-1][0]
+        if len(top5) < 5 or total_val_loss < top5last:
+            best_path = f'checkpoints/best/best_model_epoch_{epoch+1}_loss_{total_val_loss:.4f}.pth'
+            torch.save({
+                'epoch': epoch + 1, 'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(), 'val_loss': total_val_loss
+            }, best_path)
+
+            if len(top5) == 5:
+                loss_to_remove, path_to_remove = top5.pop(-1)
+                if os.path.exists(path_to_remove):
+                    os.remove(path_to_remove)
+            
+            top5.append((total_val_loss, best_path))
+    scheduler.step()
+wandb.finish()
